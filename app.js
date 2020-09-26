2898ae45377f4f21f94830d370c78e6fb852e4a6
NataHanguga <ptahakaimanka@yandex.ru>
2020-09-26 15:54:28 +0300

    fixed paths

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 // const morgan = require('morgan');
 const cors = require('cors')
 
-// const rate = require('./routes/rate');
-// const managerPosition = require('./routes/manager-position');
-// const employeePosition = require('./routes/employee-position')
-// const teacher = require('./routes/teacher')
+const rate = require('./routes/rate');
+const managerPosition = require('./routes/manager-position');
+const employeePosition = require('./routes/employee-position')
+const teacher = require('./routes/teacher')
 
 const app = express();
 
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 app.use('/', (req, res) => res.send(`<div>Hello</div>`))
-// app.use('/rate', rate);
-// app.use('/manager-position', managerPosition);
-// app.use('/employee-position', employeePosition);
-// app.use('/teacher', teacher)
+app.use('/rate', rate);
+app.use('/manager-position', managerPosition);
+app.use('/employee-position', employeePosition);
+app.use('/teacher', teacher)
 
 module.exports = app;
