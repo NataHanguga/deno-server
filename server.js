e088e61aba7f57f33e62df49f496dbc0931e179c
NataHanguga <ptahakaimanka@yandex.ru>
2020-09-26 13:36:39 +0300

    fixed for deploy

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ mongoose.connection
   });
 
 const app = require('./app');
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
