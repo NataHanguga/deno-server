16d7e543eee684ee28b77e466f4f4d4c8fa6590b
NataHanguga <ptahakaimanka@yandex.ru>
2020-09-26 13:57:08 +0300

    try to deploy

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const rate = require('./routes/rate');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors')
+// const rate = require('./routes/rate');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const cors = require('cors')
 
-const managerPosition = require('./routes/manager-position');
-const employeePosition = require('./routes/employee-position')
-const teacher = require('./routes/teacher')
+// const managerPosition = require('./routes/manager-position');
+// const employeePosition = require('./routes/employee-position')
+// const teacher = require('./routes/teacher')
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'))
-app.use(cors())
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan('dev'))
+// app.use(cors())
 
 app.use('/', (req, res) => res.send(`<div>Hello</div>`))
-app.use('/rate', rate);
-app.use('/manager-position', managerPosition);
-app.use('/employee-position', employeePosition);
-// app.use('/pedagogic-title', pedagogicTitle);
-app.use('/teacher', teacher)
+// app.use('/rate', rate);
+// app.use('/manager-position', managerPosition);
+// app.use('/employee-position', employeePosition);
+// app.use('/teacher', teacher)
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://admin:ptaha2102@workers-hya0b.mongodb.net/test?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect("mongodb+srv://admin:ptaha2102@workers-hya0b.mongodb.net/test?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-mongoose.connection
-  .on('open', () => {
-    console.log('Mongoose connection open');
-  })
-  .on('error', (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
+// mongoose.connection
+//   .on('open', () => {
+//     console.log('Mongoose connection open');
+//   })
+//   .on('error', (err) => {
+//     console.log(`Connection error: ${err.message}`);
+//   });
 
 const app = require('./app');
 const server = app.listen(process.env.PORT || 3000, () => {
